b7e10e26b45564c7e62c831e3b4402177e56bd60
BeardazardTCG <david.laird88@gmail.com>
2025-05-18 22:28:43 +0100

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,25 @@
+def filter_outliers(prices):
+    if not prices:
+        return []
+    sorted_prices = sorted(prices)
+    q1 = sorted_prices[len(sorted_prices) // 4]
+    q3 = sorted_prices[(len(sorted_prices) * 3) // 4]
+    iqr = q3 - q1
+    lower_bound = q1 - 1.5 * iqr
+    upper_bound = q3 + 1.5 * iqr
+    return [p for p in prices if lower_bound <= p <= upper_bound]
+
+def calculate_median(prices):
+    n = len(prices)
+    if n == 0:
+        return None
+    sorted_prices = sorted(prices)
+    mid = n // 2
+    if n % 2 == 0:
+        return (sorted_prices[mid - 1] + sorted_prices[mid]) / 2
+    return sorted_prices[mid]
+
+def calculate_average(prices):
+    if not prices:
+        return None
+    return sum(prices) / len(prices)
