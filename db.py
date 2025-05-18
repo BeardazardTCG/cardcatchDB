32ed4d5234dfc330fd9b47e39d03f7672318fbe9
BeardazardTCG <david.laird88@gmail.com>
2025-05-18 22:35:04 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from contextlib import asynccontextmanager
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -8,6 +9,8 @@ if not DATABASE_URL:
 engine = create_async_engine(DATABASE_URL, echo=False)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
+@asynccontextmanager
 async def get_session() -> AsyncSession:
     async with async_session() as session:
         yield session
+
